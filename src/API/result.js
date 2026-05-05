8e6ae075e8767ba05bc9695a8de5b515ccaa5471
Gaurav Gupta <143925422+codewithgaurave@users.noreply.github.com>
2026-05-05 12:03:09 +0530

    done

--- src/API/result.js
+++ src/API/result.js
@@ -57,12 +57,12 @@ export const downloadResultsByAssessmentIdApi = async (assessmentId, options = {
     }
 
     const blob = new Blob([res.data], {
-      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+      type: "text/csv;charset=utf-8;"
     });
     const url = window.URL.createObjectURL(blob);
     const a = document.createElement("a");
     a.href = url;
-    a.download = "assessment-results.xlsx";
+    a.download = "assessment-results.csv";
     document.body.appendChild(a);
     a.click();
     a.remove();
